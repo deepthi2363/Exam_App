67980d2c150794ad8467614a15a5e453fe64e9de
deepthi2363 <124274434+deepthi2363@users.noreply.github.com>
2025-08-21 18:04:57 +0530

    Update api.js with render link

--- Frontend/exam/src/api/api.js
+++ Frontend/exam/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_URL = https://exam-app-km2k.onrender.com;
 //const API_URL = "http://localhost:5000/api";
 
 // ------------------- Auth APIs -------------------
